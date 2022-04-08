e1ec50ad4e395eb4be6146d37d9217c124696e9c
mayur <mayurjkawle@gmail.com>
2022-04-08 11:58:55 +0530

    uploaded

---
+++ controller/session-controller.js
@@ -0,0 +1,49 @@
+const fs = require("fs")
+const UserModel = require("../model/user-model")
+
+function login(req,res){
+    res.write("Login")
+    res.end()
+}
+
+function signup(req,res){
+    let signupHtml = fs.readFileSync("./views/Signup.html")
+    res.write(signupHtml)
+    res.end()
+}
+
+function saveUser(req,res){
+
+    console.log(req.body)//
+
+    res.json({
+        msg:"done danadone....",
+        status:200,
+        data:req.body
+    })
+}
+
+ function sendOtpForPassword(req,res){
+        let emailParam  = req.body.email 
+        UserModel.find({email:emailParam},function(err,data){
+            if(err){
+                res.json({status:-1,msg:"SMW",data:err})
+            }else{
+                if(data.length != 0){
+                    let myotp = parseInt(Math.random()*1000000)
+                    UserModel.updateOne({email:emailParam},{otp:myotp},function(err,success){
+                        res.json({status:200,msg:"otp sent to your email",data:""})
+                    })           
+
+                }else{
+                    res.json({status:-1,msg:"Invalid Email",data:err})
+                }
+            }
+        })            
+ }
+
+
+module.exports.login = login
+module.exports.signup = signup
+module.exports.saveuser = saveUser  
+module.exports.sendOtpForPassword = sendOtpForPassword
--- index.js
+++ index.js
@@ -1,41 +1,62 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
+const sessionController = require("./controller/session-controller")
 const roleController = require("./controller/role-controller")
+const userController = require("./controller/user-controller")
 
 
 const app = express()
-//middle ware
-app.use(express.json()) //mobile -? accpet json data from request and SET DATA INTO BODY
-app.use(express.urlencoded({extended:true})) //web --> accpet url encoded data from request and set data into body
+//middle ware 
+app.use(cors())
+app.use(express.json()) //mobile -> accept json data from request and set data into body 
+app.use(express.urlencoded({extended:true})) //web --> accept url encoded data from request and set data into body  
 
 
-//database
+//database 
 mongoose.connect('mongodb://localhost:27017/TimeTracking',function(err){
-    if(err){
-        console.log("db Connection Failed......")
-        console.log(err)
-    }
-    else{
-        console.log("Database Connected")
-    }
+  if(err){
+    console.log("db connection fai .. .. . ");
+    console.log(err);
+  }else{
+    console.log("db Connected....");
+  }
 })
 
-//urls
+//urls 
+
 app.get("/",function(req,res){
-    res.write("You are welcome... Good")
+    res.write("welcome...")
     res.end()
 })
 
+          
+app.get("/login",sessionController.login) //dummy 
+app.get("/signup",sessionController.signup) //dummy
+app.post("/sendotpforpassword",sessionController.sendOtpForPassword)
 
-
-//role
+//role 
 app.post("/roles",roleController.addRole)
 app.get("/roles",roleController.getAllRoles)
-// app.delete("/roles/:roleId",roleController.deletRole)
+app.delete("/roles/:roleId",roleController.deleteRole)
+app.put("/roles",roleController.updateRole)
+app.get("/roles/:roleId",roleController.getRoleById)
 
 
-//server
-app.listen(3001,function(){
-    console.log("Server started at Localhost:3001 port")
+
+//user 
+app.post("/users",userController.addUser) //customer vendor admin 
+app.get("/users",userController.getAllUsers)
+app.delete("/users/:userId",userController.deleteUser)
+app.put("/users",userController.updateUser)
+app.post("/login",userController.login)
+
+app.post("/savecustomer",userController.addCustomer) //signup  -- customer 
+
+
+
+//server 
+app.listen(3000,function(){
+  console.log("server started on 3000");  
 })
